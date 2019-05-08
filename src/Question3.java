e4f87254d9bfa9205030224e9ac132e7a1df1e88
Jose Reginaldo <jrbj@cin.ufpe.br>
2019-05-08 07:11:52 -0300

    add complexity to question3

--- src/Question3.java
+++ src/Question3.java
@@ -6,14 +6,17 @@ public class Question3 {
 		Question3.checkTypo("pale","bale");
 		Question3.checkTypo("pale","bake");
 		Question3.checkTypo("pale","pale");
+		//Time O(n)
+		//Space O(n)
 	}
-	public static void checkTypo(String word1, String word2) {
+	public static boolean checkTypo(String word1, String word2) {
 		boolean result=false;
 		if(word1.length()==word2.length()) {result=Question3.checkLetters(word1,word2)<=1;}
 		else if(Math.abs(word1.length()-word2.length())>1) {result=false;}
 		else if(word1.length()>word2.length()) {result=Question3.checkLetterDifferentSize(word1,word2);}
 		else if(word1.length()<word2.length()) {result=Question3.checkLetterDifferentSize(word2,word1);}
-		System.out.println(word1+", "+word2+" -> "+result);		
+		System.out.println(word1+", "+word2+" -> "+result);
+		return result;		
 	}
 	public static int checkLetters(String word1, String word2) {
 		if(word1.equalsIgnoreCase(word2)) {return 0;}
