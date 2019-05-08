f77d17a6cd687957c0be0edbf29c37b7ca0546bd
Jose Reginaldo <jrbj@cin.ufpe.br>
2019-05-08 07:01:46 -0300

    add complexity to question2

--- src/Question2.java
+++ src/Question2.java
@@ -5,13 +5,16 @@ public class Question2 {
 		checkIsJumbled("probably","porbalby");
 		checkIsJumbled("despite","desptie");
 		checkIsJumbled("moon","nmoo");
-		checkIsJumbled("misspellings","mpeissngslli");		
+		checkIsJumbled("misspellings","mpeissngslli");	
+		//Space O(n)
+		//Time	O(n)
 	}
-	public static void checkIsJumbled(String rightWord,String wrongWord) {
+	public static boolean checkIsJumbled(String rightWord,String wrongWord) {
 		boolean isFirstLetterEqual = Question2.checkSameLetter(rightWord, wrongWord,0);
 		boolean isPercentageAccepted = Question2.checkPercentage(rightWord, wrongWord);
 		boolean result = isFirstLetterEqual && isPercentageAccepted;
 		System.out.println(rightWord+", "+wrongWord+"->"+result);
+		return result;
 	}
 	public static boolean checkSameLetter(String rightWord,String wrongWord,int i) {
 		return rightWord.charAt(i)==wrongWord.charAt(i);
